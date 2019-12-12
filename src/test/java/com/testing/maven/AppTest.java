5462b228cd69f4990f3415741922ee0e96274113
pavan <karthikprathyu143@gmail.com>
2019-12-12 15:59:00 +0530

    this is first commit

---
+++ src/test/java/com/testing/maven/AppTest.java
@@ -0,0 +1,35 @@
+package com.testing.maven;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+    @Test
+    public void shouldAnswerWithfalse()
+    {
+        assertTrue( true );
+    }
+    @Test
+    public void shouldAnswerWithTruee()
+    {
+        assertTrue( true );
+    }
+    @Test
+    public void shouldAnswerWithfalsee()
+    {
+        assertTrue( true );
+    }
+}
